a7977c05fd1f5a63fbaaf8c96c73078585dbdb04
Dmitry Alekseev <56576382+doctor-robot16@users.noreply.github.com>
2021-09-07 22:21:39 +0300

    Update myshell_exercise2.sh

--- myshell_exercise2.sh
+++ myshell_exercise2.sh
@@ -1,29 +1,15 @@
-#!/bin/bash
-#Упражнение 2
+/bin/bash
+echo "Directory:"
+pwd
+echo "Login:"
+whoami
 
-#include <stdlib.h>
-int main() {
-int return_value;
-return_value = system("ls -l/");
-return return_value;
-}
+echo Full Name
+read XXX
+echo My Full Name $XXX
 
-//$ system()
-//> { echo $SHELL; }
-//$ system
-// /bin/bash
-
-//echo "Directory:"
-//pwd
-//echo "Login:"
-//whoami
-
-//echo Full Name
-//read XXX
-//echo My Full Name $XXX
-
-//ls
-//pstree
-//top
-//sleep
+ls
+pstree
+top
+sleep
 
